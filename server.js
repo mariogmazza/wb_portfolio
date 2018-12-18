79434fe3eea3df57247a1f57ac7d48907422c3fc
mariogmazza <mariomazza01@gmail.com>
2018-12-18 00:53:47 -0500

    heroku staff

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get('*', (req, res) => {
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.join(__dirname, '/client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
