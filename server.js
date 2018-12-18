b2f5609725a134a26e839f22066e5078136ca283
mariogmazza <mariomazza01@gmail.com>
2018-12-18 14:17:31 -0500

    some fixes to the server

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 
 const port = process.env.PORT || 5000;
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -17,7 +18,8 @@ app.use(bodyParser.urlencoded({
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
+  // app.use(express.static(path.join(__dirname, 'client/build')));
+  app.use(express.static(path.resolve(__dirname, './client/build')));
 
   // Handle React routing, return all requests to React app
   app.get('*', (req, res) => {
@@ -26,3 +28,5 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
