5ab51325e9c4c715cc99defe90cff679e2d5dff4
mariogmazza <mariomazza01@gmail.com>
2018-12-18 00:51:24 -0500

    more build

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.post('/api/world', (req, res) => {
     res.send(`I received your POST request. This is what you sent me: ${req.body.post}`);
 });
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
